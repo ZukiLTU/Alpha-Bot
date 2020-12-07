a88f67c86c6250777768767a04895da2c95e525f
ZukiLTU <49497113+ZukiLTU@users.noreply.github.com>
2020-12-07 22:01:53 +0100

    Update IR.h

--- IR.h
+++ IR.h
@@ -1,8 +1,4 @@
-/************************************************************/
-/* Didier Petitjean le 31/10/2019/2019                      */
-/* Utilisa les capteurs IR (PCF8574) l'alphabot         */
-/*  BTS SN                                                  */
-/************************************************************/
+
 #pragma once
 #include <Arduino.h>
 #include <Wire.h>
